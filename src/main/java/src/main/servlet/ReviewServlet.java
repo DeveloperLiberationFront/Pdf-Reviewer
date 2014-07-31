d9a43eb6bacf117c2998d9341a17878706e87230
Michael Peterson <mpeterson418@gmail.com>
2014-07-31 10:25:34 -0400

    No more extra index file. I found out that GitHub will append your parameters when it gets back to you anyway, so it wasn't needed.

--- src/main/java/src/main/servlet/ReviewServlet.java
+++ src/main/java/src/main/servlet/ReviewServlet.java
@@ -1,9 +1,6 @@
 package src.main.servlet;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,23 +9,10 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ReviewServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		InputStream inst = getServletContext().getResourceAsStream("/index.html");
-		BufferedReader in = new BufferedReader(new InputStreamReader(inst));
-		
-		StringBuilder builder = new StringBuilder();
-		String line;
-		while((line = in.readLine()) != null) {
-			builder.append(line);
-		}
-		
-		in.close();
-		
-		resp.setContentType("text/html");
-		resp.getWriter().write(builder.toString());
 	}
 	
 }
