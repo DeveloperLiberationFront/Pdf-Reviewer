41b762e2a081cc5148dc122b5479b7552729e3fe
Michael Peterson <mpeterson418@gmail.com>
2014-08-13 21:45:49 -0400

    Changed the tests back to Kevin's version.

--- src/test/java/test/IssueTest.java
+++ src/test/java/test/IssueTest.java
@@ -56,25 +56,27 @@ public class IssueTest {
 	
 	@Test
 	public void testComment() {
-		assertEquals("This is an issue", a.getComment());
-		assertEquals(a.getComment(), b.getComment());
-		assertEquals(a.getComment(), c.getComment());
-		assertEquals(a.getComment(), d.getComment());
-		assertEquals(a.getComment(), e.getComment());
-		assertEquals(a.getComment(), f.getComment());
-		assertEquals(a.getComment(), g.getComment());
-		assertFalse(a.getComment() == h.getComment());
+		String aComment = a.getComment();
+		assertEquals("This is an issue", aComment);
+		assertEquals(aComment, b.getComment());
+		assertEquals(aComment, c.getComment());
+		assertEquals(aComment, d.getComment());
+		assertEquals(aComment, e.getComment());
+		assertEquals(aComment, f.getComment());
+		assertEquals(aComment, g.getComment());
+		assertFalse(aComment == h.getComment());
 	}
 	
 	@Test
 	public void testTitle() {
-		assertEquals("This is an issue", a.getTitle());
-		assertEquals(a.getTitle(), b.getTitle());
-		assertEquals(a.getTitle(), c.getTitle());
-		assertEquals(a.getTitle(), d.getTitle());
-		assertEquals(a.getTitle(), e.getTitle());
-		assertEquals(a.getTitle(), f.getTitle());
-		assertEquals(a.getTitle(), g.getTitle());
+		String aTitle = a.getTitle();
+		assertEquals("This is an issue", aTitle);
+		assertEquals(aTitle, b.getTitle());
+		assertEquals(aTitle, c.getTitle());
+		assertEquals(aTitle, d.getTitle());
+		assertEquals(aTitle, e.getTitle());
+		assertEquals(aTitle, f.getTitle());
+		assertEquals(aTitle, g.getTitle());
 	}
 	
 	@Test
