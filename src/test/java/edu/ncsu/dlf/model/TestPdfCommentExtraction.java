cff6a2249446cd2f9dda9883b4cb7d22f473a886
Kevin Lubick <kjlubick@ncsu.edu>
2015-03-22 18:41:41 -0400

    Adds stub test for mixed importances
    This will also test the UpdatePDF to see that the green/yellow/orange
    works.
    
    I also plan to migrate the other tests to use the ExpectedPDFComment

--- src/test/java/edu/ncsu/dlf/model/TestPdfCommentExtraction.java
+++ src/test/java/edu/ncsu/dlf/model/TestPdfCommentExtraction.java
@@ -6,9 +6,13 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import javax.imageio.ImageIO;
+
+import edu.ncsu.dlf.model.PdfComment.Tag;
 
 import org.junit.Test;
 
@@ -80,6 +84,27 @@ public  class TestPdfCommentExtraction {
 
     }
 
+    @Test
+    public void testCommentsOfVariousIntensities() throws Exception {
+        Pdf pdf = loadPdf("/intenseFixes.pdf");
+
+        try {
+            List<PdfComment> pdfComments = pdf.getPDFComments();
+            assertEquals(4, pdfComments.size());
+            PDFCommentMaker comments = new PDFCommentMaker()
+            .fillComments("[blank]", "Fix Please", "You might consider fixing this", "[blank]", "[blank]", "Fix Please", "You might consider fixing this")
+            .fillImages(loadExpectedImages("intenseFixes", 6))
+            .fillTitlesWithDefault();
+            comments.setTags(1, PdfComment.Tag.MUST_FIX);
+            comments.setTags(2, PdfComment.Tag.CONSIDER_FIX);
+            comments.setTags(4, PdfComment.Tag.MUST_FIX);
+            comments.setTags(5, PdfComment.Tag.CONSIDER_FIX);
+            
+            compare(comments.asList(), pdfComments, "intenseFixes");
+        } finally {
+            pdf.close();
+        }
+    }
     
 
     private Pdf loadPdf(String pathToPdf) throws IOException {
@@ -108,6 +133,85 @@ public  class TestPdfCommentExtraction {
             String possibleErrorMessage = String.format("Failed image Comparison for "+foldername+"/%d.png : difference %1.4f", i+1, difference);
             assertTrue(possibleErrorMessage, difference < 0.01);
         }
+    }
+    
+    
+    private void compare(List<ExpectedPdfComment> expected, List<PdfComment> actual, String folderName) {
+        assertEquals(expected.size(), actual.size());
+        for (int i = 0; i < actual.size(); i++) {
+            PdfComment a = actual.get(i);
+            ExpectedPdfComment e = expected.get(i);
+            assertEquals("Failed Comment Comparison for comment " + i, e.expectedComment, a.getComment());
+            assertEquals("Failed Title Comparison for comment " + i, e.expectedTitle, a.getTitle());
+            double difference = TestUtils.imagePercentDiff(e.expectedImage, a.getImage());
+            String possibleErrorMessage = String.format("Failed image Comparison for " + folderName
+                    + "/%d.png : difference %1.4f", i + 1, difference);
+            assertTrue(possibleErrorMessage, difference < 0.01);
+            assertEquals("Failed tag comparison for comment "+i, e.expectedTags, a.getTags());
+            
+        }
+    }
+
+
+    private static class PDFCommentMaker {
+        
+        private List<ExpectedPdfComment> builtComments = new ArrayList<>();
+
+        public PDFCommentMaker fillComments(String... expectedComments) {
+            for (int i = 0; i < expectedComments.length; i++) {
+                String expectedComment = expectedComments[i];
+                if (builtComments.size() <= i) {
+                    builtComments.add(new ExpectedPdfComment());
+                }
+                builtComments.get(i).expectedComment = expectedComment;
+            }
+            return this;
+        }
+
+        public List<ExpectedPdfComment> asList() {
+            validateComments();
+            return builtComments;
+        }
+
+        private void validateComments() {
+            for(ExpectedPdfComment e : builtComments) {
+                assertNotNull(e.expectedComment);
+                assertNotNull(e.expectedImage);
+                assertNotNull(e.expectedTitle);
+            }
+        }
+
+        public void setTags(int index, Tag... expectedTags) {
+            builtComments.get(index).expectedTags = Arrays.asList(expectedTags);
+        }
+
+        public PDFCommentMaker fillTitlesWithDefault() {
+            for(ExpectedPdfComment e : builtComments) {
+                e.expectedTitle = e.expectedComment;
+            }
+            return this;
+        }
+
+        public PDFCommentMaker fillImages(BufferedImage... expectedImages) {
+            for (int i = 0; i < expectedImages.length; i++) {
+                BufferedImage expectedImage = expectedImages[i];
+                if (builtComments.size() <= i) {
+                    builtComments.add(new ExpectedPdfComment());
+                }
+                builtComments.get(i).expectedImage = expectedImage;
+            }
+            return this;
+        }
+        
+    }
+    
+    private static class ExpectedPdfComment {
+
+        public List<Tag> expectedTags = new ArrayList<>();
+        public BufferedImage expectedImage;
+        public String expectedTitle;
+        public String expectedComment;
+        
     }
 
 }
