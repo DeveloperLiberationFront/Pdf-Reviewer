70a47b3fec806ae2e13c566672923413ff498d01
Justin Smith <jssmit11@ncsu.edu>
2015-04-13 14:43:28 -0400

    Revert "Optimizing index page ;)"
    This reverts commit 1a468c0adad12ee49e29edc1d934221411edc0c9.

--- src/main/java/edu/ncsu/dlf/servlet/StatusServlet.java
+++ src/main/java/edu/ncsu/dlf/servlet/StatusServlet.java
@@ -25,42 +25,39 @@ public class StatusServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.sendError(501);
+		DBAbstraction database = DatabaseFactory.getDatabase();
 
-//		DBAbstraction database = DatabaseFactory.getDatabase();
-//
-//		GitHubClient client = new GitHubClient();
-//		client.setOAuth2Token(req.getParameter("access_token"));
-//		
-//		UserService userService = new UserService(client);
-//		User user = userService.getUser();
-//			
-//		resp.setContentType("application/json");
-//
-//		try {
-//			JSONObject json = new JSONObject();
-//			
-//			List<Review> reviewsWhereUserIsRequester = database.getReviewsWhereUserIsRequester(user);
-//			List<Review> reviewsWhereUserIsReviewer = database.getReviewsWhereUserIsReviewer(user);
-//
-//			json.put("reviewsWhereUserIsReviewer", JSONUtils.toJSON(reviewsWhereUserIsReviewer));
-//			json.put("reviewsWhereUserIsRequester", JSONUtils.toJSON(reviewsWhereUserIsRequester));
-//			
-//			resp.getWriter().write(json.toString(2));
-//		} catch(Exception e) {
-//			e.printStackTrace();
-//			System.out.println("Trying to recover and send empty arrays");
-//			try {
-//                JSONObject json = new JSONObject();
-//
-//                json.put("reviewsWhereUserIsReviewer", new JSONArray());
-//                json.put("reviewsWhereUserIsRequester", new JSONArray());
-//
-//                resp.getWriter().write(json.toString(2));
-//            } catch (JSONException je) {
-//                je.printStackTrace();
-//                resp.sendError(500);
-//			}
-//		}
+		GitHubClient client = new GitHubClient();
+		client.setOAuth2Token(req.getParameter("access_token"));
+		
+		UserService userService = new UserService(client);
+		User user = userService.getUser();
+			
+		resp.setContentType("application/json");
+		try {
+			JSONObject json = new JSONObject();
+			
+			List<Review> reviewsWhereUserIsRequester = database.getReviewsWhereUserIsRequester(user);
+			List<Review> reviewsWhereUserIsReviewer = database.getReviewsWhereUserIsReviewer(user);
+
+			json.put("reviewsWhereUserIsReviewer", JSONUtils.toJSON(reviewsWhereUserIsReviewer));
+			json.put("reviewsWhereUserIsRequester", JSONUtils.toJSON(reviewsWhereUserIsRequester));
+			
+			resp.getWriter().write(json.toString(2));
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("Trying to recover and send empty arrays");
+			try {
+                JSONObject json = new JSONObject();
+
+                json.put("reviewsWhereUserIsReviewer", new JSONArray());
+                json.put("reviewsWhereUserIsRequester", new JSONArray());
+
+                resp.getWriter().write(json.toString(2));
+            } catch (JSONException je) {
+                je.printStackTrace();
+                resp.sendError(500);
+			}
+		}
 	}
 }
