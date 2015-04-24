c5b61d7a994cadecd5a229268c5ebfb416dcc3d5
Justin Smith <jssmit11@ncsu.edu>
2015-04-24 12:35:35 -0400

    Update Selenium Test

--- src/test/java/browserTest/BrowserTest.java
+++ src/test/java/browserTest/BrowserTest.java
@@ -1,10 +1,14 @@
 package browserTest;
 
+import java.io.File;
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
+
 import org.junit.*;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
@@ -27,14 +31,32 @@ public class BrowserTest {
 	}
 
 	@Test
-	public void testLogin() throws Exception {
+	public void testUploadReview() throws Exception {
 		driver.get(baseUrl + "/");
-		driver.findElement(By.id("login")).click();
-		driver.findElement(By.id("login_field")).clear();
-		driver.findElement(By.id("login_field")).sendKeys(testUserName);
-		driver.findElement(By.id("password")).clear();
-		driver.findElement(By.id("password")).sendKeys(testUserPass);
-		driver.findElement(By.name("commit")).click();
+	    driver.findElement(By.id("login")).click();
+	    driver.findElement(By.id("login_field")).clear();
+	    driver.findElement(By.id("login_field")).sendKeys(testUserName);
+	    driver.findElement(By.id("password")).clear();
+	    driver.findElement(By.id("password")).sendKeys(testUserPass);
+	    driver.findElement(By.name("commit")).click();
+	    driver.findElement(By.id("showWriterBtn")).click();
+	    driver.findElement(By.linkText("pdf-reviewer")).click();
+	    driver.findElement(By.linkText("testing-review-pdf")).click();
+	    driver.findElement(By.linkText("test.pdf")).click();
+	    driver.findElement(By.linkText("(Myself) - pdf-reviewer")).click();
+	    driver.findElement(By.id("customTags")).clear();
+	    driver.findElement(By.id("customTags")).sendKeys("[Selenium]");
+	    driver.findElement(By.id("submitReview")).click();
+	    driver.findElement(By.cssSelector("button.close")).click();
+	    driver.findElement(By.id("showStatusBtn")).click();
+	    driver.findElement(By.linkText("Review Now")).click();
+	    File file = new File("strangeAnnotations.pdf");	    
+	    driver.findElement(By.id("pdf-file")).sendKeys(file.getAbsolutePath());
+	    driver.findElement(By.id("upload")).click();
+	    driver.findElement(By.linkText("here")).click();
+	    driver.findElement(By.cssSelector("a.js-selected-navigation-item.sunken-menu-item > span.octicon.octicon-issue-opened")).click();
+	    driver.findElement(By.linkText("Selenium")).click();
+	    driver.findElement(By.cssSelector("input.js-check-all")).click();
 	}
 
 	@After
