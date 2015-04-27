fdda0e1d9f8d7ccba00acfcadcb6714bde471a97
Kevin Lubick <kjlubick@ncsu.edu>
2015-04-27 14:58:24 -0400

    Proof of concept post
    Maybe this works?

--- src/test/java/browserTest/BrowserTest.java
+++ src/test/java/browserTest/BrowserTest.java
@@ -1,17 +1,25 @@
 package browserTest;
 
+import static org.junit.Assert.fail;
+
 import java.io.File;
-import java.util.regex.Pattern;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.*;
-
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-
-import org.openqa.selenium.*;
+import org.eclipse.egit.github.core.CommitComment;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.CommitService;
+import org.eclipse.egit.github.core.service.RepositoryService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class BrowserTest {
 	private WebDriver driver;
@@ -32,13 +40,16 @@ public class BrowserTest {
 
 	@Test
 	public void testUploadReview() throws Exception {
+	    int successCounter = 0;
 		driver.get(baseUrl + "/");
+		successCounter++;
 	    driver.findElement(By.id("login")).click();
 	    driver.findElement(By.id("login_field")).clear();
 	    driver.findElement(By.id("login_field")).sendKeys(testUserName);
 	    driver.findElement(By.id("password")).clear();
 	    driver.findElement(By.id("password")).sendKeys(testUserPass);
 	    driver.findElement(By.name("commit")).click();
+	    successCounter++;
 	    driver.findElement(By.id("showWriterBtn")).click();
 	    driver.findElement(By.linkText("pdf-reviewer")).click();
 	    driver.findElement(By.linkText("testing-review-pdf")).click();
@@ -47,19 +58,43 @@ public class BrowserTest {
 	    driver.findElement(By.id("customTags")).clear();
 	    driver.findElement(By.id("customTags")).sendKeys("[Selenium]");
 	    driver.findElement(By.id("submitReview")).click();
+	    successCounter++;
 	    driver.findElement(By.cssSelector("button.close")).click();
 	    driver.findElement(By.id("showStatusBtn")).click();
+	    successCounter++;
 	    driver.findElement(By.linkText("Review Now")).click();
 	    File file = new File("strangeAnnotations.pdf");	    
 	    driver.findElement(By.id("pdf-file")).sendKeys(file.getAbsolutePath());
 	    driver.findElement(By.id("upload")).click();
+	    successCounter++;
 	    driver.findElement(By.linkText("here")).click();
 	    driver.findElement(By.cssSelector("a.js-selected-navigation-item.sunken-menu-item > span.octicon.octicon-issue-opened")).click();
 	    driver.findElement(By.linkText("Selenium")).click();
 	    driver.findElement(By.cssSelector("input.js-check-all")).click();
+	    successCounter++;
+	    
+	    reportSuccesses(successCounter);
 	}
 
-	@After
+	private void reportSuccesses(int successCounter) throws IOException {
+	    System.out.println(successCounter +" things went well");
+	    GitHubClient client = new GitHubClient();
+	    String botUserName = System.getenv("BOT_USERNAME");
+	    String botPassword = System.getenv("BOT_PASSWORD");
+	    client.setCredentials(botUserName, botPassword);
+	    
+	    String sha = System.getenv("TRAVIS_COMMIT");
+	    System.out.println("making a comment on commit "+sha);
+	    
+	    RepositoryService repoService = new RepositoryService(client);
+	    Repository repo = repoService.getRepository("DeveloperLiberationFront", "Pdf-Reviewer");
+	    CommitService service = new CommitService(client);
+	    CommitComment comment = new CommitComment();
+	    comment.setBody("test\n\n**hello**");
+        service.addComment(repo, sha, comment);
+    }
+
+    @After
 	public void tearDown() throws Exception {
 		driver.quit();
 		String verificationErrorString = verificationErrors.toString();
