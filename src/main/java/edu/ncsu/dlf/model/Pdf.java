f0a20796032266d14940a9bfc3ffad92334132ed
Santosh Mathew <scmathew@ncsu.edu>
2018-04-05 00:20:19 -0400

    Added the archive of PDF to a branch functionality

--- src/main/java/edu/ncsu/dlf/model/Pdf.java
+++ src/main/java/edu/ncsu/dlf/model/Pdf.java
@@ -9,8 +9,10 @@ import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationTextMarkup;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationText;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationRubberStamp;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationMarkup;
-import org.apache.pdfbox.pdmodel.graphics.color.PDGamma;
 
+//import org.apache.pdfbox.pdmodel.graphics.color.PDGamma;
+import org.apache.pdfbox.pdmodel.graphics.color.PDColor;
+import org.apache.pdfbox.pdmodel.graphics.color.PDDeviceRGB;
 
 import org.apache.pdfbox.rendering.PDFRenderer;
 import javax.imageio.ImageIO;
@@ -23,9 +25,9 @@ import java.util.List;
 import java.io.InputStream;
 import java.awt.image.BufferedImage;
 
-import edu.ncsu.dlf.utils.ImageUtils;;
+import edu.ncsu.dlf.utils.ImageUtils;
 
-
+import edu.ncsu.dlf.model.PdfComment.Tag;
 
 public class Pdf {
 
@@ -40,23 +42,38 @@ public class Pdf {
   private static final float SCALE_UP_FACTOR = 2.0f;
   private static final int DEFAULT_SIZE = 72;
 
-  private static final PDGamma ORANGE = new PDGamma();
-  private static final PDGamma GREEN = new PDGamma();
-  private static final PDGamma YELLOW = new PDGamma();
+//   private static final PDGamma ORANGE = new PDGamma();
+//   private static final PDGamma GREEN = new PDGamma();
+//   private static final PDGamma YELLOW = new PDGamma();
 
-  	static {
-  	    ORANGE.setR(0.9921568627f);
-  	    ORANGE.setG(0.5333333333f);
-  	    ORANGE.setB(0.1803921568f);
+  private static final PDColor ORANGE = new PDColor(
+        new float[] { 0.9921568627f, 0.5333333333f, 0.1803921568f },
+        PDDeviceRGB.INSTANCE
+    );
 
-  	    GREEN.setR(0);
-  	    GREEN.setG(1);
-  	    GREEN.setB(0);
+    private static final PDColor GREEN = new PDColor(
+        new float[] { 0, 1, 0 },
+        PDDeviceRGB.INSTANCE
+    );
 
-  	    YELLOW.setR(1);
-  	    YELLOW.setG(1);
-  	    YELLOW.setB(0);
-  	}
+    private static final PDColor YELLOW = new PDColor(
+        new float[] { 1, 1, 0 },
+        PDDeviceRGB.INSTANCE
+    );
+
+  	// static {
+  	//     ORANGE.setR(0.9921568627f);
+  	//     ORANGE.setG(0.5333333333f);
+  	//     ORANGE.setB(0.1803921568f);
+
+  	//     GREEN.setR(0);
+  	//     GREEN.setG(1);
+  	//     GREEN.setB(0);
+
+  	//     YELLOW.setR(1);
+  	//     YELLOW.setG(1);
+  	//     YELLOW.setB(0);
+  	// }
 
   public Pdf(InputStream fileStream, InputStream commentBoxImageStream) throws IOException {
     document = PDDocument.load(fileStream);
@@ -160,6 +177,73 @@ public class Pdf {
       }
       return pdfComment;
   }
+
+  public void updateCommentsWithColorsAndLinks(List<PdfComment> comments, Repo repo) {
+    PDPageTree pages = document.getDocumentCatalog().getPages();
+        int commentOn = 0;
+        for(PDPage page : pages) {
+            try {
+                List<PDAnnotation> newList = new ArrayList<PDAnnotation>();
+                List<PDAnnotation> annotations = page.getAnnotations();
+                for(int i=0; i<annotations.size(); i++) {
+                    PDAnnotation anno = annotations.get(i);
+
+                    if (anno instanceof PDAnnotationTextMarkup) {
+                        PdfComment userComment = comments.get(commentOn);
+                        commentOn++;
+                        String newMessage = userComment.getMessageWithLink(repo);
+                        PDAnnotationTextMarkup newComment = makeNewAnnotation((PDAnnotationTextMarkup) anno, userComment, newMessage);
+
+                        newList.add(newComment);
+                    } else {
+                        newList.add(anno);
+                    }
+                }
+                page.setAnnotations(newList);
+            } catch(IOException e) {
+                e.printStackTrace();
+            } finally {
+                //page.clear();
+
+                //Can't find a substitute for this, is it necessary?
+                //page.updateLastModified();
+            }
+        }
+    }
+
+    //Makes a brand new text annotation that is almost exactly like the one passed in.
+	// this is the only way I could get the annotations to actually change color.
+    private PDAnnotationTextMarkup makeNewAnnotation(PDAnnotationTextMarkup comment, PdfComment userComment, String messageWithLink) {
+        PDAnnotationTextMarkup newComment = new PDAnnotationTextMarkup(PDAnnotationTextMarkup.SUB_TYPE_HIGHLIGHT);
+        List<Tag> tags = userComment.getTags();
+        if (tags.contains(Tag.CONSIDER_FIX) || tags.contains(Tag.POSITIVE)) {
+
+            //setColour() moved to setColor()
+            //https://pdfbox.apache.org/docs/2.0.8/javadocs/org/apache/pdfbox/pdmodel/interactive/annotation/PDAnnotation.html#setColor(org.apache.pdfbox.pdmodel.graphics.color.PDColor)
+            newComment.setColor(GREEN);
+        } else if (tags.contains(Tag.MUST_FIX)) {
+            newComment.setColor(ORANGE);
+        } else {
+            newComment.setColor(YELLOW);
+        }
+        newComment.setContents(messageWithLink);
+
+        System.out.println(newComment.getContents());
+
+        newComment.setRectangle(comment.getRectangle());   //both rectangle and quadpoints are needed... don't know why
+        newComment.setQuadPoints(comment.getQuadPoints());
+        newComment.setSubject(comment.getSubject());
+        newComment.setTitlePopup(comment.getTitlePopup());    //author name
+        return newComment;
+    }
+
+    public PDDocument getDocument() {
+        return document;
+    }
+
+    public void close() throws IOException {
+	    document.close();
+	}
 
   }
 
--- src/main/java/edu/ncsu/dlf/servlet/FileUploadServlet.java
+++ src/main/java/edu/ncsu/dlf/servlet/FileUploadServlet.java
@@ -3,22 +3,46 @@ package edu.ncsu.dlf.servlet;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.codec.binary.Base64;
+
 import org.eclipse.egit.github.core.Issue;
 import org.eclipse.egit.github.core.Label;
 import org.eclipse.egit.github.core.client.GitHubClient;
 import org.eclipse.egit.github.core.service.IssueService;
+import org.eclipse.egit.github.core.service.UserService;
+import org.eclipse.egit.github.core.service.ContentsService;
+import org.eclipse.egit.github.core.service.RepositoryService;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.RepositoryContents;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.utils.URIBuilder;
+import java.io.ByteArrayOutputStream;
+import org.apache.http.entity.StringEntity;
+import org.json.JSONObject;
+import org.apache.http.impl.client.HttpClients;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+
+
+import javax.xml.bind.DatatypeConverter;
+
+import org.eclipse.egit.github.core.User;
 
 import java.io.InputStream;
 import java.io.ByteArrayInputStream;
 
 import edu.ncsu.dlf.model.Pdf;
+import edu.ncsu.dlf.model.Repo;
 import edu.ncsu.dlf.model.PdfComment;
 import edu.ncsu.dlf.model.PdfComment.Tag;
 
@@ -36,12 +60,32 @@ public class FileUploadServlet extends HttpServlet {
 		//FileUtils.writeByteArrayToFile(new File("test.pdf"), data);
 
 		InputStream commentBoxImageStream = getServletContext().getResourceAsStream(pathToCommentBoxImage);
-		
-		Pdf test = new Pdf(fileStream, commentBoxImageStream);
-		List<PdfComment> comments = test.getPDFComments();
 
+		//Instantiate GitHub Client
+		String accessToken = req.getParameter("access_token");
 		GitHubClient client = new GitHubClient();
-		client.setOAuth2Token(req.getParameter("access_token"));
+		client = client.setOAuth2Token(accessToken);
+
+
+		//Instantiate userService to get logged in username
+		UserService userService = new UserService();
+		userService.getClient().setOAuth2Token(accessToken);
+		User u = userService.getUser();
+
+		String activeUser = u.getLogin();
+		String selectedRepository = req.getParameter("selectedRepository");
+		Repo repo = new Repo(activeUser, selectedRepository);
+
+		int totalIssues = getNumTotalIssues(client, repo);
+
+		Pdf test = new Pdf(fileStream, commentBoxImageStream);
+		//List<PdfComment> comments = test.getPDFComments();
+
+		//What exactly is this doing?
+		List<PdfComment> comments = updatePdfWithNumberedAndColoredAnnotations(test, repo, totalIssues);
+
+		String selectedBranch = req.getParameter("selectedBranch");
+		String urlToPdfInRepo = addPdfToRepo(test, activeUser, selectedBranch, client, repo, accessToken);
 		
 		for(PdfComment comment: comments) {
 			System.out.println(comment.toString());
@@ -67,11 +111,112 @@ public class FileUploadServlet extends HttpServlet {
 			issue.setLabels(newLabels);
 
 			// Create the issue
-			//System.out.println("Selected Repo: " + req.getParameter("selectedRepository"));
 			//System.out.println("Selected Branch: " + req.getParameter("selectedBranch"));
             issue = issueService.createIssue("pdf-reviewer-bot", "creating-issues", issue);
 			
 			System.out.println("created issue #" + issue.getNumber());
 		}
 	}
+
+	private int getNumTotalIssues(GitHubClient client, Repo repo) throws IOException {
+	    IssueService issueService = new IssueService(client);
+
+	    Map<String, String> prefs = new HashMap<String, String>();
+	    //By default, only open issues are shown
+	    prefs.put(IssueService.FILTER_STATE, "all");
+	    //get all issues for this repo
+	    List<Issue> issues = issueService.getIssues(repo.repoOwner, repo.repoName, prefs);
+
+        return issues.size();
+	}
+	
+	private List<PdfComment> updatePdfWithNumberedAndColoredAnnotations(Pdf pdf, Repo repo, int totalIssues) throws IOException {
+	    List<PdfComment> pdfComments = pdf.getPDFComments();
+		if(!pdfComments.isEmpty()) {
+			// Set the issue numbers
+			int issueNumber = totalIssues + 1;
+			for(PdfComment com : pdfComments) {
+				if(com.getIssueNumber() == 0) {
+					com.setIssueNumber(issueNumber++);
+				}
+			}
+
+			// Update the comments to link to the repository and their newly assigned issue number
+			pdf.updateCommentsWithColorsAndLinks(pdfComments, repo);
+		}
+		return pdfComments;
+	}
+
+	private String addPdfToRepo(Pdf pdf, String activeUser, String selectedBranch, GitHubClient client, Repo repo, String accessToken) throws IOException {
+		String fileName = activeUser + "-" + java.time.LocalDate.now().toString() + ".pdf";
+		String filePath = "reviews/" + fileName;
+        String sha = null;
+        ContentsService contents = new ContentsService(client);
+        try {
+            //list all the files in reviews.  We can't just fetch our paper, because it might be
+            //bigger than 1MB which breaks this API call
+            List<RepositoryContents> files = contents.getContents(getRepo(client, repo), "reviews/");
+            for(RepositoryContents file: files) {
+                if (file.getName().equals(fileName)) {
+                    sha = file.getSha();
+                }
+            }
+        } catch(IOException e) {
+			System.out.println("No exisiting files in the reviews folder!\n");
+            //e.printStackTrace();
+        }
+
+        HttpPut request = new HttpPut(buildURIForFileUpload(accessToken, repo.repoOwner, repo.repoName, filePath));
+        try {
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            pdf.getDocument().save(output);
+
+            String content = DatatypeConverter.printBase64Binary(output.toByteArray());
+
+            JSONObject json = new JSONObject();
+            if (sha == null) {
+                //if we are uploading the review for the first time
+                json.put("message", "Archiving review of PDF: " + fileName);
+            } else {
+                //updating review
+                json.put("message", "Archiving review of PDF: " + fileName);
+                json.put("sha", sha);
+            }
+
+            json.put("path", filePath);
+			json.put("content", content);
+			json.put("branch", selectedBranch);
+
+            StringEntity entity = new StringEntity(json.toString());
+            entity.setContentType("application/json");
+            request.setEntity(entity);
+
+            HttpClients.createDefault().execute(request);
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            request.releaseConnection();
+        }
+
+        return filePath;
+	}
+	
+	private Repository getRepo(GitHubClient client, Repo repo) throws IOException {
+        RepositoryService repoService = new RepositoryService(client);
+        return repoService.getRepository(repo.repoOwner, repo.repoName);
+	}
+	
+	private URI buildURIForFileUpload(String accessToken, String writerLogin, String repoName, String filePath) throws IOException {
+        try {
+            URIBuilder builder = new URIBuilder("https://api.github.com/repos/" + writerLogin + '/' + repoName + "/contents/" + filePath);
+            builder.addParameter("access_token", accessToken);
+
+            return builder.build();
+        } catch (URISyntaxException e) {
+            throw new IOException("Could not build uri", e);
+        }
+
+    }
+
+	
 }
