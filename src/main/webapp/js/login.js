cfaad2c67af5a32156e049b8dbeeb94acd3c1da6
Santosh Mathew <scmathew@ncsu.edu>
2018-04-10 23:30:34 -0400

    Few updates to the js files to be able to better handle a change in url

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,10 +1,12 @@
+const redirect_uri = "http://localhost:9090/login";
+
 $( "#login" ).on( "click", function( event ) {
   event.preventDefault();
 
   var clientId = "c806c99f0bddb0fdd605";
   var githubAuthURL = "https://github.com/login/oauth/authorize?";
   githubAuthURL = githubAuthURL + "client_id=" + clientId + "&scope=repo&state=totallyrandomstring";
-  githubAuthURL = githubAuthURL + "&redirect_uri=" + encodeURIComponent("http://localhost:9090/login");
+  githubAuthURL = githubAuthURL + "&redirect_uri=" + encodeURIComponent(redirect_uri);
 
   window.location = githubAuthURL;
 });
