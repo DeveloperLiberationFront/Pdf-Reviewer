cfcb7ad91276190fb11cecc622d236a0c422c6eb
Pratik <pkhanal@ncsu.edu>
2018-04-26 15:34:29 -0400

    Resolved the issue with the IE and safari.

--- src/main/webapp/js/upload.js
+++ src/main/webapp/js/upload.js
@@ -53,10 +53,6 @@ $('.pdf-upload-wrap').bind('dragover', function () {
 function displayMessage(inputFile) {
     'use strict';
     var dialogButton = document.querySelector('#upload-button');
-    var dialog = document.querySelector('#dialog');
-    if (! dialog.showModal) {
-      dialogPolyfill.registerDialog(dialog);
-    }
     
     dialogButton.addEventListener('click', function() {
       var repoName = "";
@@ -93,8 +89,17 @@ function displayMessage(inputFile) {
         hideLoading();
         // var timeTakenString = "Time Taken (milliseconds):" + (performance.now() - t0) ;
         // alert(timeTakenString);
-
-        $(".mdl-dialog__content").html(data);
+        showDialog({
+          title: 'Success',
+          positive: {
+              title: 'Close'
+          },
+          cancelable: false
+          }, data);
+    //       var dialog = document.querySelector('#dialog');
+    // if (! dialog.showModal) {
+    //   dialogPolyfill.registerDialog(dialog);
+    // }
         dialog.showModal();
       });
     });
@@ -121,5 +126,76 @@ function hideLoading() {
   $('#orrsLoader').css({opacity: 0});
   setTimeout(function () {
       $('#orrsLoader').remove();
+  }, 400);
+}
+
+function showDialog(options, data) {
+  options = $.extend({
+      id: 'dialog',
+      title: null,
+      negative: false,
+      positive: false,
+      cancelable: true,
+      contentStyle: null,
+      onLoaded: false
+  }, options);
+
+  // remove existing dialogs
+  $('.dialog-container').remove();
+  $(document).unbind("keyup.dialog");
+
+  $('<div id="' + options.id + '" class="dialog-container"><div class="mdl-card hello mdl-shadow--16dp"></div></div>').appendTo("body");
+  var dialog = $('#dialog');
+  var content = dialog.find('.mdl-card');
+  if (options.contentStyle != null) content.css(options.contentStyle);
+  if (options.title != null) {
+      $('<h5>' + options.title + '</h5>').appendTo(content);
+  }
+  $(content).html(data);
+  if (options.positive) {
+      var buttonBar = $('<div class="mdl-card__actions dialog-button-bar"></div>');
+      if (options.positive) {
+          options.positive = $.extend({
+              id: 'positive',
+              title: 'OK',
+              onClick: function () {
+                  return false;
+              }
+          }, options.positive);
+          var posButton = $('<button class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" onClick="window.location.reload();" id="' + options.positive.id + '">' + options.positive.title + '</button>');
+          posButton.click(function (e) {
+              e.preventDefault();
+              if (!options.positive.onClick(e))
+                  hideDialog(dialog)
+          });
+          posButton.appendTo(buttonBar);
+      }
+      buttonBar.appendTo(content);
+  }
+  componentHandler.upgradeDom();
+  if (options.cancelable) {
+      dialog.click(function () {
+          hideDialog(dialog);
+      });
+      $(document).bind("keyup.dialog", function (e) {
+          if (e.which == 27)
+              hideDialog(dialog);
+      });
+      content.click(function (e) {
+          e.stopPropagation();
+      });
+  }
+  setTimeout(function () {
+      dialog.css({opacity: 1});
+      if (options.onLoaded)
+          options.onLoaded();
+  }, 1);
+}
+
+function hideDialog(dialog) {
+  $(document).unbind("keyup.dialog");
+  dialog.css({opacity: 0});
+  setTimeout(function () {
+      dialog.remove();
   }, 400);
 }
